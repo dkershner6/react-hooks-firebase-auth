9171bb8edb5a7c311c949492fab85920333e90b4
Derek Kershner <dkershner@cbtnuggets.com>
2020-06-22 19:47:35 -0700

    Export AuthStatus (not type)

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export type { default as IFirebaseAuthProvider } from './lib/IFirebaseAuthProvid
 export { default as FirebaseAuthContainer } from './lib/containers/FirebaseAuthContainer';
 export type { default as IFirebaseAuthContainer } from './lib/containers/IFirebaseAuthContainer';
 
-export type { AuthStatus, AuthLoadingStatuses } from './lib/common/AuthStatus';
+export { AuthStatus, AuthLoadingStatuses } from './lib/common/AuthStatus';
 
 export { default as EnforceFirebaseAuth } from './lib/wrappers/EnforceFirebaseAuth';
 export type { IEnforceFirebaseAuth } from './lib/wrappers/EnforceFirebaseAuth';
